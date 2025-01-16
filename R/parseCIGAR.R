4262ef88e7a5cbcba1638ea68c99438e871886b3
Roberto Amarie (ext) <roberto.amarie@cnode21.cm.cluster>
2025-01-16 15:56:31 +0100

    Initial commit for the parseCIGAR function

---
+++ R/parseCIGAR.R
@@ -0,0 +1,42 @@
+#' Parse a CIGAR string into its constituent operations and counts
+#'
+#' `parseCIGAR()` This function takes a CIGAR string (which describes how a sequence aligns to a reference) and splits it into individual operations (such as
+#' match, insertions, deletions, etc.) along with their corresponding counts.
+#' This function provides a structured breakdown of these operations for further analysis, such as visualizing alignments or processing genomic data.
+#'
+#' @param cigar (Mandatory) A character string representing the CIGAR string (e.g., "5M1I3M"). The string must follow standard CIGAR notation.
+#' @return A data frame with two columns:
+#' \describe{
+#'   \item{count}{A numeric vector containing the count of bases for each operation.}
+#'   \item{operation}{A character vector indicating the operation type for each segment (e.g., "M" for match, "I" for insertion).}
+#' }
+#' The resulting data frame helps to interpret each CIGAR operation and its associated base count for alignment purposes.
+#' If the input is invalid or empty, the function will return an empty data frame.
+#'
+#' @details The function uses regular expressions to split the CIGAR string into alternating numeric counts and operation types.
+#' Invalid CIGAR strings may result in an error or unexpected behavior.
+#'
+#' @examples
+#' # Example of parsing a CIGAR string:
+#' parseCIGAR("5M1I3M")
+#'
+#' @export parseCIGAR             ###### THINK MORE IF KEEPING IT INTERNAL INSTEAD
+#'
+
+
+parseCIGAR <- function(cigar) {
+
+  # parsing cigar string
+  operations <- strsplit(cigar, "(?<=\\D)(?=\\d)", perl = TRUE)[[1]]
+
+  # generating output
+  parsed <- data.frame(
+    count = as.numeric(gsub("[MIDNSHP=]", "", operations)),
+    operation = gsub("[0-9]+", "", operations),
+    stringsAsFactors = FALSE
+  )
+
+  return(parsed)
+}
+
+
